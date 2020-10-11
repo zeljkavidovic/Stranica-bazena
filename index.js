b299b374536541c5bb6229b0a7f5ee3240997b86
Zeljka Vidovic <zeljka-1987@hotmail.com>
2020-10-11 19:38:04 +0200

    sačuvala sam datoteke

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+	const current = document.querySelector('.current');
+    current.classList.remove('current');
+    
+    if(current.nextElementSibling) {
+    	current.nextElementSibling.classList.add('current');
+    } else {
+    	slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
+const prevSlide = () => {
+	const current = document.querySelector('.current');
+    current.classList.remove('current');
+    
+    if(current.previousElementSibling) {
+    	current.previousElementSibling.classList.add('current');
+    } else {
+    	slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+next.addEventListener('click', e => {
+	nextSlide();
+});
+prev.addEventListener('click', e => {
+	prevSlide();
+});
+
+
+
+
+
+
+
+
+
